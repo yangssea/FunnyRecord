050973e6bf50606d66eb0c362e190ae0fc8b74ca
zhixue.yang <zhixue.yang@aispeech.com>
2022-01-15 16:31:12 +0800

    feature：new style

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/index.scss';
-import Home from './router';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './css/font.scss'
+import './css/index.scss'
+import Home from './router'
+import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
   <React.StrictMode>
--- web/src/pages/home.tsx
+++ web/src/pages/home.tsx
@@ -26,10 +26,39 @@ export default class Home extends Component<any, any> {
                         <img  src={require('../static/find.png')} />
                     </div>
                     <div className="head">
-                        <div className="hl-sub">todo</div>
-                        <div className="hl-tab"></div>
+                        <div className="hl-sub">To-do list</div>
+                        <div className="hl-sub2">Updated on January 13</div>
+                        <div className="hl-tab">
+                            <div className="tab-child">
+                                <img  src={require('../static/do.png')} />
+                                <span>52条</span>
+                            </div>
+                            <div className="tab-child">
+                                <img  src={require('../static/ndo.png')} />
+                                <span>23条</span>
+                            </div>
+                            <div className="tab-child">
+                                <img  src={require('../static/cal.png')} />
+                                <span>52条</span>
+                            </div>
+                        </div>
                     </div>
-                    <div className="content"></div>
+                    <div className="content">
+                        <div className="do-head">
+                            <div className="do-one">
+                                <img src={require('../static/delete.png')} />
+                                <span className="title">清空全部</span>
+                                <span className="one-sum">（12）</span>
+                            </div>
+                            <div className="do-two">
+                                <img src={require('../static/download.png')} />
+                                <img src={require('../static/do1.png')} />
+                            </div>
+                        </div>
+                        <div>
+
+                        </div>
+                    </div>
                 </div>
                 <div  className="footer">
                     <BottomNavigation
